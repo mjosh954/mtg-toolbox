b752e2e6c5cf4daf2589a19a04efd6bb2a242834
jmeyers <mjosh954@gmail.com>
2015-12-28 08:51:02 -0600

    updated favicon
    - removed old tests from boilerplate
    - added favicon
    - bumped version

--- tests/layouts/CoreLayout.spec.js
+++ tests/layouts/CoreLayout.spec.js
@@ -13,21 +13,21 @@ function shallowRenderWithProps (props = {}) {
   return shallowRender(<CoreLayout {...props} />);
 }
 
-describe('(Layout) Core', function () {
-  let _component;
-  let _props;
-  let _child;
-
-  beforeEach(function () {
-    _child = <h1 className='child'>Child</h1>;
-    _props = {
-      children: _child
-    };
-
-    _component = shallowRenderWithProps(_props);
-  });
-
-  it('Should render as a <div>.', function () {
-    expect(_component.type).to.equal('div');
-  });
-});
+// describe('(Layout) Core', function () {
+//   let _component;
+//   let _props;
+//   let _child;
+//
+//   beforeEach(function () {
+//     _child = <h1 className='child'>Child</h1>;
+//     _props = {
+//       children: _child
+//     };
+//
+//     _component = shallowRenderWithProps(_props);
+//   });
+//
+//   it('Should render as a <div>.', function () {
+//     expect(_component.type).to.equal('div');
+//   });
+// });
